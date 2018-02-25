d55a922deb0e670140b3b120ce1eb7c886647049
theblackraven <theblackraven@DESKTOP-KQLU7A7>
2018-02-25 13:59:03 +0100

    Added 2 sided support

--- G-Code Converter GRBL/src/GCODE2GRBL.java
+++ G-Code Converter GRBL/src/GCODE2GRBL.java
@@ -10,12 +10,14 @@ import javax.swing.JTextArea;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollBar;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.filechooser.FileFilter;
+import java.util.Scanner;
 
 public class GCODE2GRBL {
 
@@ -51,7 +53,7 @@ public class GCODE2GRBL {
 		frmTargetToGrbl = new JFrame();
 		frmTargetToGrbl.setTitle("Target3001 to GRBL");
 		frmTargetToGrbl.getContentPane().setBackground(Color.LIGHT_GRAY);
-		frmTargetToGrbl.setBounds(100, 100, 903, 567);
+		frmTargetToGrbl.setBounds(100, 100, 1239, 567);
 		frmTargetToGrbl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTargetToGrbl.getContentPane().setLayout(null);
 		
@@ -96,8 +98,16 @@ public class GCODE2GRBL {
 		scrollPane_1.setBounds(555, 45, 322, 473);
 		frmTargetToGrbl.getContentPane().add(scrollPane_1);
 		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane_2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollPane_2.setBounds(887, 45, 322, 473);
+		frmTargetToGrbl.getContentPane().add(scrollPane_2);
+		
 		JTextArea textArea_1 = new JTextArea();
 		scrollPane_1.setViewportView(textArea_1);
+		JTextArea textArea_2 = new JTextArea();
+		scrollPane_2.setViewportView(textArea_2);
 		
 		JButton btnUmwandeln = new JButton("Umwandeln");
 		btnUmwandeln.addActionListener(new ActionListener() {
@@ -114,9 +124,24 @@ public class GCODE2GRBL {
 				Sentence = Sentence.replace("M08", "M8");
 				Sentence = Sentence.replace("M09", "M9");
 				Sentence = Sentence.replace("G71", "G21");
-				Sentence = Sentence.replaceAll("\n{2,}","\n");
+			
 				
-				textArea_1.setText(Sentence);
+				Sentence = Sentence.replaceFirst("(?m)^M6.*", "");
+
+				
+				String[] splitSentence = Sentence.split("(MSG, TURN OVER PCB NOW AND CONTINUE)");
+				splitSentence[0] = splitSentence[0].replace("(", "");
+				splitSentence[0] = splitSentence[0].replace(")", "");
+				splitSentence[0] = splitSentence[0].replaceAll("(?m)^\\s*$[\n\r]{1,}", "");
+				splitSentence[0] = splitSentence[0] + "M2\nM30";
+				splitSentence[1] = splitSentence[1].replace("(", "");
+				splitSentence[1] = splitSentence[1].replace(")", "");
+				splitSentence[1] = "G21\nG90\nG94\nM5\nM9\n" + splitSentence[1];
+				splitSentence[1] = splitSentence[1].replaceAll("(?m)^\\s*$[\n\r]{1,}", "");
+				splitSentence[0] = splitSentence[0].replace("\n", "\r\n");
+				splitSentence[1] = splitSentence[1].replace("\n", "\r\n");
+				textArea_1.setText(splitSentence[0]);
+				textArea_2.setText(splitSentence[1]);
 				
 				
 			}
@@ -128,10 +153,7 @@ public class GCODE2GRBL {
 		btnSpeichern.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFrame frame = new JFrame();
-				int answer = JOptionPane.showConfirmDialog(frame,"Ersten Werkzeugwechsel entfernt? (M6)");
 				
-				if (answer==JOptionPane.YES_OPTION)
-				{
 				
 				JFileChooser fs = new JFileChooser(new File(""));
 				fs.setDialogTitle("geänderten G-Code speichern");
@@ -139,20 +161,24 @@ public class GCODE2GRBL {
 				if (result== JFileChooser.APPROVE_OPTION){
 					File fi = fs.getSelectedFile();
 					try{
-						FileWriter fw = new FileWriter(fi.getPath());
+						FileWriter fw = new FileWriter(fi.getPath()+"Front.nc");
 						fw.write(textArea_1.getText());
 						fw.flush();
 						fw.close();
+						
+						FileWriter fw1 = new FileWriter(fi.getPath()+"Back.nc");
+						fw1.write(textArea_2.getText());
+						fw1.flush();
+						fw1.close();
 						JOptionPane.showMessageDialog(frame, "Erfolgreich gespeichert");					
 						
-					} catch (Exception e2){
-						JOptionPane.showMessageDialog(null,  e2.getMessage());
+						} catch (Exception e2){
+							JOptionPane.showMessageDialog(null,  e2.getMessage());
 						
+						}
+						
+					
 					}
-					
-					
-				}
-				}
 				}
 			});
 		btnSpeichern.setBounds(384, 468, 109, 23);
